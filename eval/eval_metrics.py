ffd91483a81808ac3bb1b32edfa488a59996ed32
Jessica Wang <jessiwang2000@gmail.com>
2025-04-17 00:56:15 -0700

    basic readability scores

--- eval/eval_metrics.py
+++ eval/eval_metrics.py
@@ -1,4 +1,5 @@
 from rouge_score import rouge_scorer
+from readability import Readability
 
 def get_rouge_scores(gold: str, test: str) -> dict:
     """
@@ -8,3 +9,20 @@ def get_rouge_scores(gold: str, test: str) -> dict:
     scorer = rouge_scorer.RougeScorer(['rouge1', 'rouge2', 'rougeL'], use_stemmer=True)
     score = scorer.score(gold, test)
     return score
+
+def get_readability_scores(text: str) -> dict:
+    """
+    Gets a dictionary of readability scores and other metadata for a given string after tokenizing.
+    """
+    r = Readability(text)
+    return {
+        "flesch_kincaid": r.flesch_kincaid(),
+        "flesch": r.flesch(),
+        "gunning_fog": r.gunning_fog(),
+        "coleman_liau": r.coleman_liau(),
+        "dale_chall": r.dale_chall(),
+        "ari": r.ari(),
+        "linsear_write": r.linsear_write(),
+        "smog": r.smog(),
+        "spache": r.spache()
+    }
