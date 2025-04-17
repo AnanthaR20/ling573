f5afc3d059a9c43c62ae2579e84b9c9100d3b112
Jessica Wang <jessiwang2000@gmail.com>
2025-04-17 00:56:41 -0700

    filled out .txt and .csv functionality on sample for system testing

--- preprocess/sample.py
+++ preprocess/sample.py
@@ -1,6 +1,30 @@
 from datasets import load_dataset, train_test_split
+import argparse
 
 def sample(n=100):
     ds = load_dataset("FiscalNote/billsum")
-    sample = ds.train_test_split(test_size=n, seed=42)["test"]
-    return sample.to_pandas()
+    sample_data = ds.train_test_split(test_size=n, seed=42)["test"]
+    return sample_data.to_pandas()
+
+def to_text():
+    sample()["text"].to_csv("sample_text.txt", header=False, index=False)
+    sample()["summary"].to_csv("sample_summary.txt", header=False, index=False)
+    return
+
+def to_csv():
+    sample()["text"].to_csv("sample_text.csv", header=False, index=False)
+    sample()["summary"].to_csv("sample_summary.csv", header=False, index=False)
+    return 
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--to_txt", help="Returns text column and summary column as .txt files")
+    parser.add_argument("--to_csv", help="Returns text column and summary column as .csv files")
+    args = parser.parse_args()
+
+    if args.to_txt:
+        to_text()
+    elif args.to_csv:
+        to_csv()
+    else:
+        print("oh dear")
