aef44d596aa0a8b643604ce0d7481ce13e9e00b2
Anantha <anantha@anr3.local>
2025-04-25 16:01:55 -0700

    Added run baseline cmd files and shell scripts

---
+++ run_baseline/run_baseline.cmd
@@ -0,0 +1,10 @@
+executable = run_baseline.sh
+getenv = true
+error = run_baseline.error
+log = run_baseline.log
+output = run_baseline.out
+notification = complete
+arguments = ""
+transfer_executable = false
+request_memory = 16000
+queue
