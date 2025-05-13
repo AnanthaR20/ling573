e2ff03f878d57840b38d72383c1a30b6d1481103
Jessica Wang <jessiwang2000@gmail.com>
2025-05-12 18:54:13 -0700

    update python path

--- finetune_model.sh
+++ finetune_model.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 # /home2/anjiniv/miniconda3/bin/python3.12 load_data.py
-/home2/$USER/miniconda3/envs/torch312/bin/python finetune_model.py
+/home2/$USER/miniconda3/envs/573-env/bin/python finetune_model.py
