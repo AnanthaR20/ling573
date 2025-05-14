cbc6055085542dff8da20e8e4b8adf2c6f3e5774
Anantha <anantha@anr3.local>
2025-05-13 22:18:32 -0700

    added samples of fkre quantiles

--- eval/get_fkre_quantile_reps.R
+++ eval/get_fkre_quantile_reps.R
@@ -52,16 +52,45 @@ num_quantiles = 5
 gold_quantiles <- lftk$gold %>% mutate(quantile = ntile(fkre,num_quantiles))
 gen_quantiles <- lftk$gen %>% mutate(quantile = ntile(fkre,num_quantiles))
 
-smps <- c()
+smps_gold <- c()
+smps_gen <- c()
 for(i in 1:num_quantiles){
   df_gold <- gold_quantiles %>% filter(quantile == i)
-  smps <- c(smps,sample(df_gold$X,1))
+  df_gen <- gen_quantiles %>% filter(quantile == i)
+  smps_gold <- c(smps_gold,sample(df_gold$X,1))
+  smps_gen <- c(smps_gen,sample(df_gen$X,1))
 }
 
-t <- gold_quantiles %>% filter(X %in% smps)
-t2 <- t %>% left_join(gen_quantiles,by="X",suffix = c(".GOLD",".GEN"))
+t_gold <- gold_quantiles %>% filter(X %in% smps_gold)
+t2_gold <- t_gold %>% left_join(gen_quantiles,by="X",suffix = c(".GOLD",".GEN"))
 
-write.csv(t2,file = "output_rows_for_5_fkre_quantiles.csv",row.names = F)
+t_gen <- gen_quantiles %>% filter(X %in% smps_gen)
+t2_gen <- t_gen %>% left_join(gold_quantiles,by="X",suffix = c(".GEN",".GOLD"))
+
+write.csv(t2_gold,file = "gold_fkre_quantiles.csv",row.names = F)
+write.csv(t2_gen,file = "gen_fkre_quantiles.csv",row.names = F)
+
+
+
+# get samples of
+
+a <- read.csv("output_rows_where_gold_tword_above250.csv")
+a_quantiles <- a %>% mutate(fkre_quantile.GOLD = ntile(fkre.GOLD,5),fkre_quantile.GEN = ntile(fkre.GEN,5))
+# View(a_quantiles)
+gold_quants <- c()
+gen_quants <- c()
+for(i in 1:5){
+     df_gold <- a_quantiles %>% filter(fkre_quantile.GOLD == i)
+     df_gen <- a_quantiles %>% filter(fkre_quantile.GEN == i)
+     gold_quants <- c(gold_quants,sample(df_gold$X,1))
+     gen_quants <- c(gen_quants,sample(df_gen$X,1))
+}
+
+a_quantiles %>% filter(X %in% gold_quants) %>% 
+  write.csv(file = "samples_where_gold_tword_above250.csv")
+
+# a_quantiles %>% filter(X %in% gen_quants) %>% 
+#   write.csv(file = "gen_quantile_samples_where_gold_tword_above250.csv")
 
 
 
