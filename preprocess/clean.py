c6d96782d0f545d8cf54a56f5fd86803808fabc7
Jessica Wang <jessiwang2000@gmail.com>
2025-05-19 12:30:30 -0700

    refactored clean step

--- preprocess/clean.py
+++ preprocess/clean.py
@@ -1,1 +1,32 @@
-#TODO: This script will eventually be called in the main shell script
+import pandas as pd
+import re
+from datasets import load_dataset
+import argparse
+
+SECTION_HEADER_RE = re.compile(r'\b(SEC(?:TION)?\.?\s*\d+[A-Za-z]?\.*.*?)\n+')
+SUBSECTION_RE = re.compile(r'\(\w+\)\s*')
+PARENTH_RE = re.compile(r'\n\s*\(\d+\)\s*')
+STRIP_RE = re.compile(r'\s+')
+
+def clean_text(example):
+    example["text"] = SECTION_HEADER_RE.sub('', example["text"])
+    example["text"] = SUBSECTION_RE.sub('', example["text"])
+    example["text"] = PARENTH_RE.sub(' ', example["text"])
+    example["text"] = STRIP_RE.sub(' ', example["text"]).strip()
+    return example
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--dataset", default="FiscalNote/billsum", help="specify HuggingFace dataset")
+    parser.add_argument("--split", default=["train", "test"])
+    parser.add_argument("--output_file",default=None,help="overrides default naming schema")
+    args = parser.parse_args()
+
+    ds = load_dataset(args.dataset, split = args.split)
+    ds = ds.map(clean_text)
+    
+    for split, dataset in ds.items():
+        dataset.to_csv(f"{args.dataset}_{split}_clean.csv", index=None)
+
+if __name__ == "__main__":
+    main()
