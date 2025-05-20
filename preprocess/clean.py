3f6062395c59e8ddd97edb41b80492873d7102b4
Jessica Wang <jessiwang2000@gmail.com>
2025-05-20 02:50:52 -0700

    convert to df before writing

--- preprocess/clean.py
+++ preprocess/clean.py
@@ -37,7 +37,8 @@ def main():
             curr_name = curr_name.split(".")[0] + "_toy.csv"
             dataset = dataset.select(range(args.toy))
         dataset = dataset.map(clean_text)
-        dataset[:, ["text", "summary"]].to_csv(curr_name, index=None, escapechar="\\")
+        df = dataset.to_pandas()
+        df.to_csv(curr_name, index=None, usecols=["text", "summary"], escapechar="\\")
 
 if __name__ == "__main__":
     main()
