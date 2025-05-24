6c7be2fff7166d2ac080864777c6584e0172b8de
Anantha <anantha@anr3.local>
2025-05-23 17:05:57 -0700

    Added flag to skip lftk computation for gold_text(since it's already in eval/gold_lftk.csv in the repo)

--- eval/eval_metrics.py
+++ eval/eval_metrics.py
@@ -70,7 +70,8 @@ def eval_all(
   include_rouge:bool = True,
   include_read_formula:bool = True,
   include_word_sent:bool = True,
-  include_word_diff:bool = False
+  include_word_diff:bool = False,
+  skip_gold_lftk:bool = True
 ) -> dict[str,float|int]:
   """Gets evaluation metrics for a pair of texts. Intended to receive a gold and
   generated summary and output rouge scores for the pair and lftk feature scores
@@ -85,6 +86,7 @@ def eval_all(
       family of metrics
     include_word_sent: whether to include the lftk 'word_sent' family of metrics
     include_word_diff: whether to include the lftk 'word_diff' family of metrics 
+    skip_gold_lftk: flag to indicate if we should skip lftk metrics for 'gold_text'
 
   Returns:
     a dictionary of all the requested metrics for the given pair of 
@@ -103,7 +105,8 @@ def eval_all(
   lftk_features = _readformula + _wordsent + _worddiff
   
   # For gold
-  results.update(eval_lftk(gold_text,lftk_features,suffix=".GOLD"))
+  if not skip_gold_lftk:
+    results.update(eval_lftk(gold_text,lftk_features,suffix=".GOLD"))
 
   # For gen
   results.update(eval_lftk(gen_text,lftk_features,suffix=".GEN"))
