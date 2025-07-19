d8372bedb337ecec071ebbc0a749550290d61d2e
Anantha Rao <anantha.nalin.rao@gmail.com>
2025-07-18 19:19:10 -0700

    Working on the cmd file

---
+++ eval/align_score/align_score.cmd
@@ -0,0 +1,9 @@
+executable = align_score.sh
+getenv     = true
+error      = hw5.error
+log        = hw5.log
+output     = hw5.out
+notification = complete
+transfer_executable = false
+request_memory = 2*1024
+queue
