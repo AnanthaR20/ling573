fabfb38f857ecb69c7379bd154421bbff04ab43b
Anantha Rao <anantha.nalin.rao@gmail.com>
2025-07-21 10:31:02 -0700

    updated command script to have output

--- eval/align_score/align_score.cmd
+++ eval/align_score/align_score.cmd
@@ -2,6 +2,7 @@ executable = align_score.sh
 getenv     = true
 error      = align_score.error
 log        = align_score.log
+output     = align_score_dummy.output
 notification = complete
 transfer_executable = false
 request_memory = 4*1024
