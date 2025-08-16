d7efe08757e6b2061ade892dbea7d57c6173e139
Jessica Wang <jessiwang2000@gmail.com>
2025-08-16 16:20:42 -0700

    update stored submodule and final binary normal inference + evaluation job details

--- Binary_Evaluate.cmd
+++ Binary_Evaluate.cmd
@@ -1,11 +1,11 @@
 ############
-# LED-base, 2048 input (+ATS, +blanks)
+# LED-base, 1024 input (-ATS, + binary blanks)
 ############
 executable = nllp_evaluate_model.sh
 getenv = true
 environment = "CLUSTER_ID=$(Cluster); PROCESS_ID=$(Process);"
 arguments = --config_id 4 --base_tokenizer allenai/led-base-16384 \
---checkpoint /home2/jcmw614/ling573/models/led-base/billsum_clean_train_se3-led-1024-512/binary_blank_targets/1024_512_5_epochs/checkpoint-/ \
+--checkpoint /home2/jcmw614/ling573/models/led-base/billsum_clean_train_se3-led-1024-512/binary_blank_targets/1024_512_5_epochs/checkpoint-108471/ \
 --mode test \
 --testfile preprocess/nllp_data/billsum_clean_test_se3-led-1024-512.csv \
 --batch_size 6 
