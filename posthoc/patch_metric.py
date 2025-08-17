18a58a286b399c7ab9db1afa5c21587e0a4420b5
Jessica Wang <jessiwang2000@gmail.com>
2025-08-16 18:50:45 -0700

    improve behavior for gold vs prediction handling

--- posthoc/patch_metric.py
+++ posthoc/patch_metric.py
@@ -7,40 +7,64 @@ import pandas as pd
 import argparse
 import warnings
 
-def compute(metric_name, ds, batch_size):
+def compute(metric_name: str, is_gold: bool, ds: Dataset, batch_size:int):
+    """
+    Args:
+        metric_name: metric to generate
+        is_gold: whether the target column is 'summary' or 'predicted_summary'
+        ds: Dataset
+        batch_size: batch size, if mapping
+    Returns:
+        Dataset with new metric column
+    """
     # Assign metric names to functions
     metric_fns = {
         "bertscore": metrics.get_bertscore_metrics,
         "redundancy": metrics.get_redundancy_scores,
-        "alignscore": metrics.eval_alignscore_batch,
+        "alignscore": metrics.eval_alignscore_tmp,
         "summac": metrics.eval_summac_batch
     }
     func = metric_fns[metric_name]
 
-    # Redundancy is only measured in aggregate
-    if metric_name == "redundancy":
-        _, _, _, _ = func(ds["predicted_summary"])
-    # BERTScore is batched and always requires preds + refs
-    elif metric_name == "bertscore":
-        ds =ds.map(
-            lambda ex: func(ex["predicted_summary"], ex["summary"])
-        )
+    if is_gold:
+        target_column = "summary"
     else:
-    # AlignScore and SummaC use text and predicted_summary
+        target_column = "predicted_summary"
+    
+    # Redundancy is only computed in aggregate
+    if metric_name == "redundancy":
+        _, _, _, _ = func(ds[target_column])
+    # BERTScore always requires preds + refs
+    elif metric_name == "bertscore":
         ds = ds.map(
-            func,
+            lambda ex: func(ex["predicted_summary"], ex["summary"]),
+            batched=True,
+            batch_size=batch_size
+        )
+    else: # Alignscore and SummaC compare texts to target column
+        ds = ds.map(
+            lambda ex: func(ex["text"], ex[target_column]),
             batched=True,
             batch_size=batch_size
         )
     return ds
 
 def load_data(source_file:str, metric_name:str):
+    """
+    Args:
+        source_file: source filepath
+        metric_name: metric to generate and 'patch'
+    Returns:
+        Huggingface Dataset for mapping metric and boolean to represent if data is 'gold'
+    """
+    # Check if this is gold or prediction data
+    is_gold = "gold" in source_file
     # Read as dataframe
     df = pd.read_csv(source_file)
     if metric_name in df.columns:
         warnings.warn("Metric already exists in source file; this job will overwrite it!")
     # Convert to HF Dataset
-    return Dataset.from_pandas(df)
+    return Dataset.from_pandas(df), is_gold
 
 def load_args():
     parser = argparse.ArgumentParser()
@@ -58,10 +82,10 @@ def main():
     print("Running on ", device)
 
     # Load input file
-    data = load_data(args.file, args.metric)
+    data, is_gold = load_data(args.file, args.metric)
 
     # Compute metric
-    data = compute(args.metric, data, args.batch_size)
+    data = compute(args.metric, is_gold, data, args.batch_size)
 
     # Overwrite file
     data.to_csv(args.file)
