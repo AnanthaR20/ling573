d3e74b40ecb73bb4a272cbd30869bcb2e2cdf651
Jessica Wang <jessiwang2000@gmail.com>
2025-08-16 21:33:37 -0700

    reorder finetune jobs by size

--- Binary_Finetune.cmd
+++ Binary_Finetune.cmd
@@ -1,21 +1,4 @@
 ############
-# LED-base, 1024 input (-ATS)
-############
-executable = nllp_finetune_model.sh
-getenv = true
-environment = "CLUSTER_ID=$(Cluster); PROCESS_ID=$(Process);"
-arguments = --checkpoint allenai/led-base-16384 --mode train --trainfile preprocess/nllp_data/billsum_clean_train_se3-led-1024-512.csv --batch_size 4 --blank_targets binary
-transfer_executable = false
-output = binary_phase_2.$(Cluster).$(Process).out
-error = binary_phase_2.$(Cluster).$(Process).err
-log = binary_phase_2.$(Cluster).$(Process).log
-request_GPUs = 1
-Requirements = (Machine == "patas-gn3.ling.washington.edu")
-request_memory = 3000
-notification = error
-queue
-
-############
 # LED-base, 2048 input (+ATS)
 ############
 executable = nllp_finetune_model.sh
@@ -28,6 +11,23 @@ error = binary_phase_2.$(Cluster).$(Process).err
 log = binary_phase_2.$(Cluster).$(Process).log
 request_GPUs = 1
 Requirements = (Machine == "patas-gn3.ling.washington.edu")
-request_memory = 3000
+request_memory = 5000
+notification = error
+queue
+
+############
+# LED-base, 1024 input (-ATS)
+############
+executable = nllp_finetune_model.sh
+getenv = true
+environment = "CLUSTER_ID=$(Cluster); PROCESS_ID=$(Process);"
+arguments = --checkpoint allenai/led-base-16384 --mode train --trainfile preprocess/nllp_data/billsum_clean_train_se3-led-1024-512.csv --batch_size 4 --blank_targets binary
+transfer_executable = false
+output = binary_phase_2.$(Cluster).$(Process).out
+error = binary_phase_2.$(Cluster).$(Process).err
+log = binary_phase_2.$(Cluster).$(Process).log
+request_GPUs = 1
+Requirements = (Machine == "patas-gn3.ling.washington.edu")
+request_memory = 6000
 notification = error
 queue
