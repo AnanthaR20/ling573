e1c45f5b5be3135a6368bbb1d0c37330512f2124
Jessica Wang <jessiwang2000@gmail.com>
2025-08-19 13:35:56 -0700

    fix filepath aha

--- nllp_postprocess_evaluate.py
+++ nllp_postprocess_evaluate.py
@@ -98,7 +98,7 @@ def load_chunk_level_predictions(config_id: int):
 
     print("Loading source chunk-level inputs...")
     # Load input data
-    _, _, source_data = load_data(f"tmp/nllp_data/billsum_clean_test_{metadata['se3_config']}.csv")
+    _, _, source_data = load_data(f"preprocess/nllp_data/billsum_clean_test_{metadata['se3_config']}.csv")
 
     print("Loading document IDs for second-pass inference...")
     # Load document IDs with empty summaries
