e29fd618972c5ccd92c2ed91a371360b70b53a2e
Jessica Wang <jessiwang2000@gmail.com>
2025-08-18 17:01:08 -0700

    first pass condor cmd to get D2 results aligned on our NLLP work

---
+++ posthoc/patch_pegasus.cmd
@@ -0,0 +1,44 @@
+############
+# Deliverable 2: Redundancy
+############
+executable = posthoc/patch_metric.sh
+getenv = true
+arguments = --file output/pegasusbillsum_baseline_ALL_metrics.csv \
+--metric redundancy --batch_size 8
+transfer_executable = false
+output = patch_metric.pegasus.alignscore.out
+error = patch_metric.pegasus.alignscore.err
+log = patch_metric.pegasus.alignscore.log
+request_memory = 1000
+notification = error
+queue
+
+############
+# Deliverable 2: BERTScore
+############
+executable = posthoc/patch_metric.sh
+getenv = true
+arguments = --file output/pegasusbillsum_baseline_ALL_metrics.csv \
+--metric bertscore --batch_size 8
+transfer_executable = false
+output = patch_metric.pegasus.bertscore.out
+error = patch_metric.pegasus.bertscore.err
+log = patch_metric.pegasus.bertscore.log
+request_memory = 4000
+notification = error
+queue
+
+############
+# Deliverable 2: Alignscore
+############
+executable = posthoc/patch_metric.sh
+getenv = true
+arguments = --file output/pegasusbillsum_baseline_ALL_metrics.csv \
+--metric alignscore --batch_size 8
+transfer_executable = false
+output = patch_metric.pegasus.alignscore.out
+error = patch_metric.pegasus.alignscore.err
+log = patch_metric.pegasus.alignscore.log
+request_memory = 4000
+notification = error
+queue
