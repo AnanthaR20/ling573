407a073e10340c65f0baf6d30cda40a44041d9c9
Jessica Wang <jessiwang2000@gmail.com>
2025-08-21 16:27:30 -0700

    remove batch size arg from condor cmd

--- posthoc/patch_summac.cmd
+++ posthoc/patch_summac.cmd
@@ -4,7 +4,7 @@
 executable = posthoc/patch_metric.sh
 getenv = true
 arguments = --file output/0.led-base.billsum_clean_train_se3-led-1024-512.drop_blank_targets.1024_512_5_epochs.checkpoint-41832.csv \
---metric summac --batch_size 1
+--metric summac 
 transfer_executable = false
 output = patch_metric.0.summac.out
 error = patch_metric.0.summac.err
@@ -21,7 +21,7 @@ queue
 executable = posthoc/patch_metric.sh
 getenv = true
 arguments = --file output/12.led-base.billsum_clean_train_se3-led-2048-512.binary_blank_targets.2048_512_5_epochs.checkpoint-45220.csv \
---metric summac --batch_size 1
+--metric summac 
 transfer_executable = false
 output = patch_metric.12.summac.out
 error = patch_metric.12.summac.err
@@ -38,7 +38,7 @@ queue
 executable = posthoc/patch_metric.sh
 getenv = true
 arguments = --file output/13.led-base.billsum_clean_train_se3-led-1024-512.binary_blank_targets.1024_512_5_epochs.checkpoint-108471.csv \
---metric summac --batch_size 1
+--metric summac 
 transfer_executable = false
 output = patch_metric.13.summac.out
 error = patch_metric.13.summac.err
@@ -55,7 +55,7 @@ queue
 executable = posthoc/patch_metric.sh
 getenv = true
 arguments = --file output/deliverable_4/led-base/led-base_billsum_clean_test_se3-led-2048-512.csv \
---metric summac --batch_size 1
+--metric summac 
 transfer_executable = false
 output = patch_metric.13.summac.out
 error = patch_metric.13.summac.err
@@ -72,7 +72,7 @@ queue
 executable = posthoc/patch_metric.sh
 getenv = true
 arguments = --file output/1.led-base.billsum_clean_train_se3-led-2048-512_simple.drop_blank_targets.2048_512_5_epochs.checkpoint-22804.csv \ 
---metric summac --batch_size 1
+--metric summac 
 transfer_executable = false
 output = patch_metric.1.summac.out
 error = patch_metric.1.summac.err
