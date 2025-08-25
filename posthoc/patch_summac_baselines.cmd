66f4ab8882ff23712141c820861195c6890da017
Jessica Wang <jessiwang2000@gmail.com>
2025-08-24 22:07:48 -0700

    Condor file to compute factuality for D2 and D4

---
+++ posthoc/patch_summac_baselines.cmd
@@ -0,0 +1,44 @@
+############
+# Deliverable 4: AlignScore
+############
+executable = posthoc/patch_metric.sh
+getenv = true
+arguments = --file output/deliverable_4/led-base/led-base_billsum_clean_test_se3-led-2048-512.csv \
+--metric alignscore --batch_size 8 
+transfer_executable = false
+output = patch_metric.d4.alignscore.out
+error = patch_metric.d4.alignscore.err
+log = patch_metric.d4.alignscore.log
+request_memory = 4000
+notification = error
+queue
+
+############
+# Deliverable 2: SummaC
+############
+executable = posthoc/patch_metric.sh
+getenv = true
+arguments = --file output/deliverable_2/pegasusbillsum_baseline_ALL_metrics.csv \
+--metric summac 
+transfer_executable = false
+output = patch_metric.pegasus.summac.out
+error = patch_metric.pegasus.summac.err
+log = patch_metric.pegasus.summac.log
+request_memory = 4000
+notification = error
+queue
+
+############
+# Deliverable 4: SummaC
+############
+executable = posthoc/patch_metric.sh
+getenv = true
+arguments = --file output/deliverable_4/led-base/led-base_billsum_clean_test_se3-led-2048-512.csv \
+--metric summac 
+transfer_executable = false
+output = patch_metric.d4.summac.out
+error = patch_metric.d4.summac.err
+log = patch_metric.d4.summac.log
+request_memory = 4000
+notification = error
+queue
